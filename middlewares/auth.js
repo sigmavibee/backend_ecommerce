e385862ecdcce19a4717ad8caedd15703dcc12b1
unknown <rizkyrn027@gmail.com>
2025-05-22 18:37:23 +0700

    new backend conf

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,12 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) return res.status(401).json({ message: 'Access denied: No token' });
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ message: 'Access denied: No token' });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Invalid token', error: err.message });
-    req.user = user;
-    next();
-  });
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token', error: err.message });
+        req.user = user;
+        next();
+    });
 };
